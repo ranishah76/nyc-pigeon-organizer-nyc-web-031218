ce37e32ed3f3d57a77e9cf1c8acf8b0a416612e5
Rani Shah <rani76@gmail.com>
2018-03-01 12:59:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organised = {}
+    data.each do |trait, options|
+      options.each do |option, names|
+        names.each do |name|
+          organised[name] ||= {}
+          organised[name][trait] ||= []
+          organised[pigeon][trait] << option.to_s
+        end
+      end
+    end
+      organised
+
 end
